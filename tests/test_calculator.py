0352e01624643ca4ffc22c206809fc340f4b8c61
Leah Moshe <moshelea123@gmail.com>
2026-01-19 18:31:39 +0200

    Failing tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -13,3 +13,16 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         divide(10, 0)
+
+
+# Failing tests
+def test_add_fail():
+    assert add(2, 2) == 5  
+
+
+def test_divide_fail():
+    assert divide(9, 3) == 2  
+
+
+def test_divide_by_zero_fail():
+    divide(5, 1)
